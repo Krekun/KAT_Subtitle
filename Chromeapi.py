90704d08408877e0b1ccd48032da568884f63311
Krekun <baryonss@gmail.com>
2022-06-19 14:23:29 +0900

    翻訳機能実装OK

--- Chromeapi.py
+++ Chromeapi.py
@@ -19,14 +19,14 @@ class Websocket():
         server = WebsocketServer(port=5001, host='127.0.0.1')
         server.set_fn_new_client(self.new_client)
         server.set_fn_message_received(self.message_received)
-        url="http://localhost:8001/"
-        webbrowser.open(url)
+        # url="http://localhost:8001/"
+        # webbrowser.open(url)
         server.run_forever()
     def new_client(self,client, server):
         print ("接続しました")
 
     def message_received(self,client, server, message):
-        # print (message)
+        print (message)
         self.q_sentence.put(message)
 
 if __name__=="__main__":
